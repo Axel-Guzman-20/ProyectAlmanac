9e0dfd3491788ed6cc5516d0bd58d2b290c28a70
laloCastro22 <eduardocastroramon0@gmail.com>
2022-12-21 00:06:57 -0600

    Segundo commit, HU-07 codigo y con pruebas unitarias

--- src/main/java/mx/uam/ingsof/proyecto/ProyectoApplication.java
+++ src/main/java/mx/uam/ingsof/proyecto/ProyectoApplication.java
@@ -210,19 +210,19 @@ public class ProyectoApplication {
 		empleadoPrueba.setCorreoElectronico("Eduardo@gmail.com");
 		empleadoRepository.save(empleadoPrueba);
 		
-		// SE REGISTRAN EMPLEADOS EN AUTOMÁTICO
-		servicioCliente.registrarCliente("27/11/2022", "Benito Camelo", "Masculino", "Calle 10 Alc. Roma, CDMX, CDMX", "5536521474", "cliente1@correo.com");
-		servicioCliente.registrarCliente("27/11/2022", "Benito Ramos", "Masculino", "Calle 10 Alc. Roma, CDMX, CDMX", "5536521474", "cliente1@correo.com");
-		servicioCliente.registrarCliente("26/11/2022", "Elver Gonzales", "Masculino", "Calle 11 Alc. Roma, CDMX, CDMX", "5585967414", "cliente2@correo.com");
-		servicioCliente.registrarCliente("25/11/2022", "Rosa Mela", "Femenino", "Calle 12 Alc. Roma, CDMX, CDMX", "5536963696", "cliente3@correo.com");
-		servicioCliente.registrarCliente("24/11/2022", "Elva Ginon", "Femenino", "Calle 13 Alc. Roma, CDMX, CDMX", "5512547854", "cliente4@correo.com");
+		
 		/*
 		// SE REGISTRAN EMPLEADOS EN AUTOMÁTICO
 		controladorEmpleado.registraEmpleado("Abigail Morales", "M", "abigail@correo.com", "1234567891", "Calle 1, Col, Roma, Alc Carranza, CDMX, CDMX");
 		controladorEmpleado.registraEmpleado("Miguel Guzman", "H", "miguel@correo.com", "5896479625", "Calle 2, Col, Roma, Alc Carranza, CDMX, CDMX");
 		controladorEmpleado.registraEmpleado("Axel Guzman", "H", "axel@correo.com", "2563149563", "Calle 3, Col, Roma, Alc Carranza, CDMX, CDMX");
 		controladorEmpleado.registraEmpleado("Eduardo Castro", "H", "eduardo@correo.com", "9674852893", "Calle 4, Col, Roma, Alc Carranza, CDMX, CDMX");
-		
+		// SE REGISTRAN EMPLEADOS EN AUTOMÁTICO
+		servicioCliente.registrarCliente("27/11/2022", "Benito Camelo", "Masculino", "Calle 10 Alc. Roma, CDMX, CDMX", "5536521474", "cliente1@correo.com");
+		servicioCliente.registrarCliente("27/11/2022", "Benito Ramos", "Masculino", "Calle 10 Alc. Roma, CDMX, CDMX", "5536521474", "cliente1@correo.com");
+		servicioCliente.registrarCliente("26/11/2022", "Elver Gonzales", "Masculino", "Calle 11 Alc. Roma, CDMX, CDMX", "5585967414", "cliente2@correo.com");
+		servicioCliente.registrarCliente("25/11/2022", "Rosa Mela", "Femenino", "Calle 12 Alc. Roma, CDMX, CDMX", "5536963696", "cliente3@correo.com");
+		servicioCliente.registrarCliente("24/11/2022", "Elva Ginon", "Femenino", "Calle 13 Alc. Roma, CDMX, CDMX", "5512547854", "cliente4@correo.com");
 		// SE REGISTRAN VENTAS EN AUTOMÁTICO
 		ArrayList<VentaProducto> listaProductos = new ArrayList<VentaProducto>(3);
 		
